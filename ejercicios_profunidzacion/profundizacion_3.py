1bbca5a3915438b37db652aec12575e370ba6b5f
mfranice <mfranice@gmail.com>
2021-05-12 21:04:55 -0300

    Ejercicio 3 de profundización finalizado

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -19,11 +19,11 @@ Tome el ejercicio de "calificaciones":
 copielo a este ejercicio y modifíquelo para cumplir
 el siguiente requerimiento
 
-Las notas del estudinte se encuentran almacenadas en una
+Las notas del estudiante se encuentran almacenadas en una
 lista llamada "notas" que ya hemos definido al comienzo del archivo
 
-Debe caluclar el promedio de todas las notas y luego transformar
-la califiación en una letra según la escala establecida en el ejercicio
+Debe calcular el promedio de todas las notas y luego transformar
+la calificación en una letra según la escala establecida en el ejercicio
 "calificaciones" <condicionales_python / ejercicios_practica / ejercicio_3.py>
 
 A medida que recorre las notas, no debe considerar como válidas aquellas
@@ -35,9 +35,11 @@ Debe contar la cantidad de notas válidas y la cantidad de ausentes
 print("Mi organizador académico (#_#)")
 # Empezar aquí la resolución del ejercicio
 
+notas = [70, 100, 75, 60, 100, -1, 20]
+
 # Para calcular el promedio primero debe obtener la suma
 # de todas las notas, que irá almacenando en esta variable
-sumatoria = 0           # Ya le hemos inicializado en 0
+sumatoria = 0           # Ya la hemos inicializado en 0
 
 cantidad_notas = 0      # Aquí debe contar cuantas notas válidas encontró
 cantidad_ausentes = 0   # Aquí debe contar cuantos ausentes hubo
@@ -45,10 +47,47 @@ cantidad_ausentes = 0   # Aquí debe contar cuantos ausentes hubo
 # Realice aquí el bucle para recorrer todas las notas
 # y cacular la sumatoria
 
+for nota in notas:
+    
+    sumatoria += nota
+
+    if nota > 0:
+        cantidad_notas += 1
+    else:
+        cantidad_ausentes +=1
+
 # Terminado el bucle calcule el promedio como
 # promedio = sumatoria / cantidad_notas
+
+promedio = sumatoria / cantidad_notas
+
+print('La cantidad de notas es:', cantidad_notas)
+print('El promedio de las notas del estudiante es:', promedio)
 
 # Utilice la nota promedio calculada y transformela
 # a calificación con letras, imprima en pantalla el resultado
 
+# Si el promedio es mayor igual a 90 --> imprimir A
+# Si el promedio es mayor igual a 80 --> imprimir B
+# Si el promedio es mayor igual a 70 --> imprimir C
+# Si el promedio es mayor igual a 60 --> imprimir D
+# Si el promedio es menor a  60      --> imprimir F
+
+if promedio >= 90:
+    print('La nota final del estudiante es: A')
+elif promedio >= 80:
+    print('La nota final del estudiante es: B')
+elif promedio >= 70:
+    print('La nota final del estudiante es: C')
+elif promedio >= 60:
+    print('La nota final del estudiante es: D')
+else:
+    print('La nota final del estudiante es: F')
+
 # Imprima en pantalla al cantidad de ausentes
+
+print ('La cantidad de ausencias del estudiante es:', cantidad_ausentes)
+
+
+
+
